de6bd360caf92bc5d17a44f532a268096dd8af6a
maslias <marcel.liebreich@gmail.com>
2025-01-15 12:51:28 +0100

    progress

--- setup.sh
+++ setup.sh
@@ -4,10 +4,21 @@ user="marciii-devpod"
 userpw="passwd"
 
 # add user
-sudo useradd -m -G wheel -s /bin/zsh $user
+# sudo useradd -m -G wheel -s /bin/zsh $user
 # add password
-echo "$user:$userpw" | sudo chpasswd
+# echo "$user:$userpw" | sudo chpasswd
 
 # echo '%wheel ALL=(ALL:ALL) ALL' >/etc/sudoers.d/wheel
 # sudo echo '%wheel ALL=(ALL:ALL) ALL' >/etc/sudoers.d/wheel
 # sudo adduser $user wheel
+#
+#
+#
+
+rm ~/.zshrc
+
+cd ~/dotfiles
+rm -rf .git
+
+stow .
+
