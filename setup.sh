856bb326333885574bc7b15f6dfe38a1da514a61
maslias <marcel.liebreich@gmail.com>
2025-01-15 12:31:57 +0100

    progress

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 user="marciii-devpod"
+userpw="passwd"
 
 # add user
 sudo useradd -m -G wheel -s /bin/zsh $user
+# add password
+echo "$user:$userpw" | sudo chpasswd
+
 # echo '%wheel ALL=(ALL:ALL) ALL' >/etc/sudoers.d/wheel
-sudo echo '%wheel ALL=(ALL) ALL' >/etc/sudoers.d/wheel
-sudo adduser $user wheel
+sudo echo '%wheel ALL=(ALL:ALL) ALL' >/etc/sudoers.d/wheel
+# sudo adduser $user wheel
